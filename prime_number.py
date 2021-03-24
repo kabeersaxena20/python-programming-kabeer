29cbe7567ec5c2f5953ca4bfc178f68820dd9c5f
kabeersaxena20 <80817213+kabeersaxena20@users.noreply.github.com>
2021-03-24 14:19:06 +0530

    Update prime_number.py

--- prime_number.py
+++ prime_number.py
@@ -1,10 +1,2 @@
-import time
-n=int(input('Enter The Number'))
-for i in range(2,n):
-    if i % n == 0:
-        print("Not a Prime.")
-        break
-else:
-    print('Prime NO.')
-    
-time.sleep(3)
+
+
